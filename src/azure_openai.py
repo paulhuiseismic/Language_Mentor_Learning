36d61e997959dde9e717593114e761859f0923b7
Paul Y Hui <paulhui@seismic.com>
2025-11-21 08:54:14 +0800

    Implemented the conversation agent and run it successfully.

---
+++ src/azure_openai.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from openai import AzureOpenAI
+from langchain_openai import AzureChatOpenAI
+
+load_dotenv()
+
+client = AzureOpenAI(
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_API_VERSION"),
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+)
+
+chat_model = AzureChatOpenAI(
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+    api_version=os.getenv("AZURE_API_VERSION"),
+    azure_deployment=os.getenv("AZURE_MODEL"),
+)
